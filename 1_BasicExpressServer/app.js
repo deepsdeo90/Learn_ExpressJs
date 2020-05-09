e35e7a704378aa02c49bc8d12fe5b5027717a04a
Dipali <deeps0310@gmail.com>
2020-05-08 17:23:02 -0700

    json download and post

--- 1_BasicExpressServer/app.js
+++ 1_BasicExpressServer/app.js
@@ -1,8 +1,55 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const app = express();
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+//load static html
 app.use(express.static(path.join(__dirname, 'public')));
+
+//load Json
+app.get('/users',(req, res) => {
+    const users = [
+      {
+          "name": "Leanne Graham",
+          "username": "Bret",
+          "email": "Sincere@april.biz",
+      },
+      {
+          "name": "Ervin Howell",
+          "username": "Antonette",
+          "email": "Shanna@melissa.tv",
+      },
+      {
+          "name": "Clementine Bauch",
+          "username": "Samantha",
+          "email": "Nathan@yesenia.net",
+        }
+    ];
+    res.json(users);
+});
+
+//download file
+app.get('/download', (req, res) => {
+  res.download(path.join(__dirname, 'downloads/sample.pdf'));
+});
+
+//redirect
+app.get('/about', (req,res) =>{
+  res.redirect('/about.html');
+});
+
+//post
+app.post('/submit', (req, res)=>{
+  const name = req.body.name;
+  const email = req.body.email;
+  console.log(name+' has subscribed with '+email);
+});
 
 app.listen(3000, function(){
   console.log('server started in port 3000');
